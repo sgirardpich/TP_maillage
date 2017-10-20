603e2c73eaf04ec0f9fe49828705c4df0aeb4267
GIRARD-PICHOUD Sophie <sgirardpich@chauvet.pedago.ipb.fr>
2017-10-20 08:49:06 +0200

    Mesh.cpp

---
+++ Mesh.cpp
@@ -0,0 +1,20 @@
+#ifndef _MESH_CPP
+
+#include "Mesh.h"
+#include <fstream>
+#include <iostream>
+#include <assert.h>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+using namespace Eigen;
+
+Mesh::Mesh(string meshname)
+: _meshname(meshname)
+{
+}
+
+
+#define _MESH_CPP
+#endif
